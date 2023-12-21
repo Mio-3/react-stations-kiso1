d72305bd7ed21afb8f5d666817ebf71a852743e4
Kazuma <21kazuma21@gmail.com>
2023-12-21 22:56:42 +0900

    APIの取得が出来たと思う

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'react';
 
-const App = () => {
 
-  return (
-    <div>
-      <h1>掲示板</h1>
-    </div>
+
+
+export const App = () => {
+    const [threads,setThreads] = useState([]);
+    useEffect(() => {
+      fetch("https://railway.bulletinboard.techtrain.dev/threads")
+      .then(response => response.json())
+      .then(threads => console.log(threads))
+    });
+  
+    return (
+      <div>
+        <h1>掲示板</h1>
+        <header>新着スレッド</header>
+      </div>
   )
 };
+
 
 export default App;
